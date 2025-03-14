49a0f17b5c0027faff24252e694079d69f756fee
itaysaban <itaysaban21@gmail.com>
2025-03-14 11:23:13 +0200

    styled leaderboard + fixed bugs

--- src/ClickerGame.js
+++ src/ClickerGame.js
@@ -6,13 +6,13 @@ import LeaderBoard from './LeaderBoard.js'
 function ClickerGame() {
 
     const [timer, setTimer] = useState(null);
-    const [points, setPoints] = useState(0);
     const [countDown, setCountDown] = useState(null);
     const [difficulty, setDifficulty] = useState(null);
     const [currentPoints, setCurrentPoints] = useState(0)
+    const [savedScore, setSavedScore] = useState([])
 
     const startGame = () => {
-      setPoints(0);
+      setCurrentPoints(0);
       setCountDown(3);
         if (difficulty === "Easy") {
           setTimer(30);
@@ -28,18 +28,18 @@ function ClickerGame() {
 
 
     const stopGame = () => {
-      setPoints(0);
+      setCurrentPoints(0);
       setCountDown(null);
       setTimer(null);
         };
 
     const addPoints = () => {
-        setPoints((prevPoints) => prevPoints + 1);
+        setCurrentPoints((prevPoints) => prevPoints + 1);
     };
 
     const setGameDifficulty = (level) => {
       setDifficulty(level);
-      setPoints(0);
+      setCurrentPoints(0);
       setTimer(null);
       setCountDown(null);
     }
@@ -79,7 +79,7 @@ function ClickerGame() {
     return (
         <div style={{ textAlign: "center", marginTop: "50px" }}>
           <h1 id="title">Clicker Game</h1>
-          <div class="game-background">
+          <div className="game-background">
           {timer === 0 || timer === null ? null : (
             <h2 id="time">
               {countDown !== null && countDown > 0 ? (
@@ -89,25 +89,25 @@ function ClickerGame() {
               )}
             </h2>
     )}
-          {timer === 0 ? <h2>Game Over, you scored {points} points! Start again!</h2>: <h2>Click the button as much as possible!</h2>}
-          {timer !== 0 &&<h2>Points: {points}</h2>}
+          {timer === 0 ? <h2>Game Over, you scored {currentPoints} points! Start again!</h2>: <h2>Click the button as much as possible!</h2>}
+          {timer !== 0 &&<h2>Points: {currentPoints}</h2>}
           {(timer === null || timer === 0) && (
                 <div>
               <h3>Choose difficulty:</h3>
-                <div class="difficulties">
+                <div className="difficulties">
                   <button id="easy" onClick={() => setGameDifficulty("Easy")}>Easy</button>
                   <button id="medium" onClick={() => setGameDifficulty("Medium")}>Medium</button>
                   <button id="hard" onClick={() => setGameDifficulty("Hard")}>Hard</button>
                 </div>
               </div>)}
-          <div class="gameButtons">
+          <div className="gameButtons">
             <button id="startGame" onClick={startGame} disabled={timer !== null && timer > 0} >
               Start Game
             </button>
             <button id="clickMe" onClick={addPoints} disabled={(timer === 0 || timer === null)|| countDown !== null}>
               Click Me!
           </button>
-          <LeaderBoard currentPoints={currentPoints} />
+          <LeaderBoard currentPoints={currentPoints} timer={timer} />
           {(timer !== null) &&
           <button id="reset" onClick={stopGame}>Reset</button>}
           </div>
--- src/LeaderBoard.js
+++ src/LeaderBoard.js
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from "react";
+import './LeaderBoard.css'
 import './ClickerGame.js';
 
-const LeaderBoard = ({currentPoints}) => {
+const LeaderBoard = ({currentPoints, timer}) => {
     const [score, setScore] = useState([]);
 
     useEffect(() => {
         const savedScores = JSON.parse(localStorage.getItem('leaderboard')) || [];
         setScore(savedScores)
-    })
+    }, [])
     
     useEffect(() => {
-        if (currentPoints > 0) {
+        if ((currentPoints > 0) && (timer === 0)) {
             setScore((prevScores) => {
                 const updatedScores = [...prevScores, currentPoints]
                     .sort((a, b) => b - a)
                     .slice(0, 10);
-                
                 localStorage.setItem('leaderboard', JSON.stringify(updatedScores));  // Save updated scores to localStorage
                 return updatedScores;
             });
         }
-    }, [currentPoints]); 
+    }, [timer]); 
 
 
     return (
-        <div>
-            <h1 id="leaderboardtitle">Highest Points Glory Board</h1>
+        <div className="leaderboard">
+            <h3 className="leaderboardtitle">Highest Points Glory Board</h3>
                 <ol id="scorelist">
                     {score.map((score, index) =>(
-                        <li key={index}>{score} points</li>
+                        <li key={index} className="leaderboard-item">{score} points</li>
                     ))}
                 </ol>
         </div>
